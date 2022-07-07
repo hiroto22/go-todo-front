f123e0a1ab5fde6d9d53df4fc2ce6e355f283f32
hiroto22 <tochihirototo.nya.49@gmail.com>
2022-07-07 18:15:25 +0900

    heroku

--- src/pages/topPage.tsx
+++ src/pages/topPage.tsx
@@ -2,15 +2,22 @@ import { Header } from "../components/organisms/header";
 import styled from "styled-components";
 import { Todos } from "../components/organisms/todos";
 import { AddIcon } from "../components/atoms/addIcon";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 export const TopPage = () => {
   const navigate = useNavigate();
+  const token = "Bearer " + sessionStorage.getItem("token");
   return (
-    <div>
-      <Header />
-      <Todos />
-      <AddIcon onClick={() => navigate("/addtodo")} />
-    </div>
+    <>
+      {token !== "Bearer null" ? (
+        <div>
+          <Header />
+          <Todos />
+          <AddIcon onClick={() => navigate("/addtodo")} />
+        </div>
+      ) : (
+        <Navigate to="login" />
+      )}
+    </>
   );
 };
