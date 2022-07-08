0b0728d1f08cf9f415a8fffaee3b64baf4865377
hiroto22 <tochihirototo.nya.49@gmail.com>
2022-07-08 21:15:43 +0900

    heroku

--- src/hooks/useCompleteTodo.ts
+++ src/hooks/useCompleteTodo.ts
@@ -11,17 +11,17 @@ export const useCompleteTodo = () => {
       .catch((err) => console.log(err));
   };
 
-  const ReturnTodo = (token: string, id: number) => {
+  const ReturnTodo = async (token: string, id: number) => {
     const URL = `${process.env.REACT_APP_URL}/completetodo?id=${id}&isComplete=true`;
-    axios
+    await axios
       .post(URL, null, { headers: { Authorization: token } })
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
   };
 
-  const DeleteTodo = (token: string, id: number) => {
+  const DeleteTodo = async (token: string, id: number) => {
     const URL = `${process.env.REACT_APP_URL}/deletetodo?id=${id}`;
-    axios
+    await axios
       .post(URL, null, { headers: { Authorization: token } })
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
