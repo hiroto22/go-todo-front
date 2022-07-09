ee83a4795945443b08cd700175be7b9c72776da1
hiroto22 <tochihirototo.nya.49@gmail.com>
2022-07-09 21:27:40 +0900

    heroku

--- src/components/organisms/login.tsx
+++ src/components/organisms/login.tsx
@@ -26,8 +26,7 @@ export const Login = () => {
             <Grid item justifyContent="center">
               <p>
                 新規登録の方は
-                <a href={`${process.env.REACT_APP_FRONT_URL}/signup`}>こちら</a>
-                へ
+                <a href={"/signup"}>こちら</a>へ
               </p>
             </Grid>
             <Grid item>
