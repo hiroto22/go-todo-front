53a3563fc796c2ed4a03bbe8e31cd65b8c9fb238
hiroto22 <tochihirototo.nya.49@gmail.com>
2022-07-11 09:24:49 +0900

    heroku

--- src/hooks/useCreateTodo.ts
+++ src/hooks/useCreateTodo.ts
@@ -3,11 +3,11 @@ import axios from "axios";
 export const useCreateTodo = () => {
   const URL = `${process.env.REACT_APP_URL}/createtodo`;
 
-  const CreateTodo = (token: string, todo: any) => {
+  const CreateTodo = async (token: string, todo: any) => {
     const data = { todo: todo };
     console.log(data);
 
-    axios
+    await axios
       .post(URL, JSON.stringify(data), { headers: { Authorization: token } })
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
